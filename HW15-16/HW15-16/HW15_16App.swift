70b3818e630b7b6570df8ded975363025b2a85ca
Bohdan <borysenkobohdan@gmail.com>
2023-09-26 16:14:53 +0300

    create_project

---
+++ HW15-16/HW15-16/HW15_16App.swift
@@ -0,0 +1,17 @@
+//
+//  HW15_16App.swift
+//  HW15-16
+//
+//  Created by Богдан Борисенко on 26.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct HW15_16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
